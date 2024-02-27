0dcca3e53e280b3cda45b4a440338daf7c42d576
David Dulce <davidulce26@gmail.com>
2024-02-27 15:31:57 +0000

    Add java project

---
+++ src/main/java/dev/arkantrust/cards/CarbonCards.java
@@ -0,0 +1,11 @@
+package dev.arkantrust.cards;
+
+public class CarbonCards {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Carbon Cards!");
+
+    }
+
+}
---
+++ src/test/java/dev/arkantrust/cards/CarbonCardsTest.java
@@ -0,0 +1,11 @@
+package dev.arkantrust.cards;
+
+public class CarbonCardsTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Testing: Carbon Cards");
+
+    }
+
+}
